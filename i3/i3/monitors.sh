190f1a9d9714cae712582546dd49cbcf8a01051f
Adrian-George Bostan <adrg@epistack.com>
2025-11-01 16:02:28 +0200

    Update i3 config

---
+++ i3/i3/monitors.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+xrandr --output DP-2 --primary --mode 2560x1440 --rate 59.95 --output DP-3 --left-of DP-2 --mode 2560x1440 --rate 59.95 --rotate right --output DP-4 --right-of DP-2 --mode 2560x1440 --rate 59.95 --rotate left
